11d0796d263c28832834e4771cdd908b09e3aa9c
Stanley Pan <stanleypan.egre@gmail.com>
2016-05-21 16:27:51 +0900

    Initial Commit

---
+++ AlertExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertExercise
+//
+//  Created by Stanley Pan on 5/21/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlertExercise/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AlertExercise
+//
+//  Created by Stanley Pan on 5/21/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
